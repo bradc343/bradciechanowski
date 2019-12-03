9a5341c9195e861cb9282c00e87d38e976d71384
Brad Ciechanowski <bradc343@gmail.com>
2019-12-03 14:17:37 -0500

    Add files via upload

---
+++ precache-manifest.279f4ca6e24e2611ccee0d2141d26dbd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21778d89260c74c1244ea8635dce34c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "931752594e7fc799d877",
+    "url": "/static/css/main.b2c4f65e.chunk.css"
+  },
+  {
+    "revision": "1ca827ff519a9a84db3a",
+    "url": "/static/js/2.90336022.chunk.js"
+  },
+  {
+    "revision": "931752594e7fc799d877",
+    "url": "/static/js/main.ea095d7f.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  },
+  {
+    "revision": "d3e1e1f42a6e6bad85b4fbf0f2c5e702",
+    "url": "/static/media/bartender.d3e1e1f4.jpg"
+  },
+  {
+    "revision": "8d14941bbda54bf373e76f8bf7d66d83",
+    "url": "/static/media/brad.8d14941b.jpg"
+  },
+  {
+    "revision": "948f755856db1a9c44cec86fbe17e1c1",
+    "url": "/static/media/falls.948f7558.jpg"
+  },
+  {
+    "revision": "baf8d17e25a504f4444c0e64bbb0a25f",
+    "url": "/static/media/signature.baf8d17e.svg"
+  },
+  {
+    "revision": "d91d7bf1f011d59f4711c620a9ef38c9",
+    "url": "/static/media/tiger.d91d7bf1.jpg"
+  }
+]);
